acc12a20e10e988e251a1a14733cf5d45bd43100
jcahela <jpacahela@gmail.com>
2024-04-01 12:58:19 -0500

    Add images and create exercise table sql file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-24">
+      <Image
+        src="/images/Air_Bike_0.jpg"
+        alt="Air Bike"
+        width={500}
+        height={500}
+      />
     </main>
   );
 }
