6277817f2abe2c478936034f32fa184582962ddd
jcahela <jpacahela@gmail.com>
2024-04-03 11:38:10 -0500

    Remove db connection string from WeightWiseContext.OnConfiguration method into Program.cs startup file, use Configuration variables st ored in appsettings.json for safer storage

--- back/Program.cs
+++ back/Program.cs
@@ -1,9 +1,15 @@
+using back.Models;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WeightwiseContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DevConString")));
 
 var app = builder.Build();
 
